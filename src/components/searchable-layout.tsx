0d88e153c0cd12c934802243bb795df72509adc6
yammyam <tkdxo999@gmail.com>
2025-05-31 02:04:00 +0900

    서치바 기능구현

--- src/components/searchable-layout.tsx
+++ src/components/searchable-layout.tsx
@@ -1,13 +1,45 @@
-import { ReactNode } from "react";
-
+import { useRouter } from "next/router";
+import { ReactNode, useEffect } from "react";
+import { useState } from "react";
 export default function SearchableLayout({
   children,
 }: {
   children: ReactNode; //타입설정 들오는 녀석은 "인덱스페이지"임, 상세페이지같은녀석은 아님.
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const q = router.query.q as string; //타입단언as
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    //엔터눌렀을때작동하는함수
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div>
-      <div>임시서치바</div>
+      <div>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요."
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
       {children}
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { NextPage } from "next";
 // getLayout은 우리가 커스텀스럽게 만든 함수기때문에 타입별칭을 통해 타입을 명시해줘야 함
 // NextPage는 넥스트에서 제공하는 페이지컴포넌트의 기본타입
 type NextWithLayout = NextPage & {
-  getLayout: (page: ReactNode) => ReactNode;
+  getLayout?: (page: ReactNode) => ReactNode;
 };
 
 export default function App({
