d8f33a851522f0b297c65fbb9e881b86246b23a3
yammyam <tkdxo999@gmail.com>
2025-05-31 01:44:42 +0900

    타입별칭 및 선택적 서치바

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import GlobalLayout from "@/components/global-layout";
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
+import { NextPage } from "next";
+// getLayout은 우리가 커스텀스럽게 만든 함수기때문에 타입별칭을 통해 타입을 명시해줘야 함
+// NextPage는 넥스트에서 제공하는 페이지컴포넌트의 기본타입
+type NextWithLayout = NextPage & {
+  getLayout: (page: ReactNode) => ReactNode;
+};
 
-export default function App({ Component, pageProps }: AppProps) {
-  console.log(Component.getLayout);
-  return (
-    <GlobalLayout>
-      <Component {...pageProps} />
-    </GlobalLayout>
-  );
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,4 +1,6 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 // ?q=홍길동  쿼리스트링은 경로에 대해 뭘 건들지 않기 때문에 파일구조엔 변화없다.
 export default function Page() {
   const router = useRouter();
@@ -6,3 +8,6 @@ export default function Page() {
   const { q } = router.query;
   return <h1>{q}</h1>;
 }
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
