4768dce5b010e44ce6e6abe60f0c2df64709e839
yammyam <tkdxo999@gmail.com>
2025-05-31 21:56:54 +0900

    모든페이지 스타일링설정

--- src/pages/book/[[...id]].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Page() {
-  const router = useRouter();
-  console.log(router);
-  const { id } = router.query;
-  console.log(id);
-  return <h1>book 개별페이지 {id}번째 책</h1>;
-}
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,16 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 // ?q=홍길동  쿼리스트링은 경로에 대해 뭘 건들지 않기 때문에 파일구조엔 변화없다.
 export default function Page() {
-  const router = useRouter();
-  console.log(router);
-  const { q } = router.query;
-  return <h1>{q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 Page.getLayout = (page: ReactNode) => {
   return <SearchableLayout>{page}</SearchableLayout>;
