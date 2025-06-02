86622749346639819b2c7a469c5166a955331fd7
yammyam <tkdxo999@gmail.com>
2025-06-02 23:58:34 +0900

    ssr실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
-import books from "../mock/books.json";
+import books from "@/mock/books.json"; // ../목/북제이슨하면 오류남 404
 import BookItem from "@/components/book-item";
 // @/mock/books.json 경로에서 골뱅이는 src를 가리키는뜻, package.json에서 확인가능
 
-export default function Home() {
-  // ㅁㄴㅇㄹ
+export const getServerSideProps = () => {
+  //컴퍼넌트보다 먼저 실행되어서,컴포넌트에 필요한(백엔드데이터) 데이터를 미리 불러오는 함수
+  const data = "hello";
+  return {
+    props: {
+      data,
+    },
+  };
+};
+export default function Home({ data }: any) {
+  console.log(data);
   return (
     <div className={style.container}>
       <section>
