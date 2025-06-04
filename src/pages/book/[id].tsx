1acd3f5483158d5ca360b1ff068438a81256c2a0
yammyam <tkdxo999@gmail.com>
2025-06-04 20:46:01 +0900

    책 설명페이지 SSG

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,10 +1,27 @@
+// [id] 명칭은 dynamicPath
+// 다이나믹 패스를 SSG로 생성하려하면 오류나는 이유 : 어떤페이지가올지 모르기때문에 따로 id를 몇개 설정해줘서 생성해야하는 방식으로 해야함
+// 그역할을 하는 함수가 getStaticPaths
 import fetchOneBook from "@/lib/fetch-one-book";
 import style from "@/pages/book/[id].module.css";
-import { GetServerSidePropsContext, InferGetStaticPropsType } from "next";
+import {
+  GetServerSidePropsContext,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: false,
+    //예비상황에 대한 대비책 느낌. ex)정해두지않은 파람스 값
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id; //!는 id가 있을것이다 라는 명시, 파일명이 [id]라서 파람스가 있을수밖에없음.(있어야 접근할수있어서)
   const book = await fetchOneBook(Number(id));
   return {
@@ -14,7 +31,7 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetStaticPropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) {
     return <p>문제가 발생했습니다. 다시 시도해주세요</p>;
   }
